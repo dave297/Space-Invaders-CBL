0b637d8b74450a54d942ee402bc48b083bb06397
Quang <vuquang2030@gmail.com>
2025-10-14 11:50:53 +0200

    Add functional game's menu

--- Window.java
+++ Window.java
@@ -1,22 +1,40 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class Window extends JFrame {
-    private final GamePanel gamePanel = new GamePanel();
+
+    private final CardLayout cardLayout = new CardLayout(); 
+    private final JPanel root = new JPanel(cardLayout);
+    private GamePanel gamePanel = new GamePanel();
+    private GameMenu gameMenu;
 
 
     public Window() {
         this.setTitle("Space Invaders");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setContentPane(gamePanel);
-        pack();
-        this.setLocationRelativeTo(null);
         gamePanel.setFocusable(true);
         gamePanel.requestFocusInWindow();
+        gameMenu = new GameMenu(this);
+        root.add(gameMenu, "menu");
+        root.add(gamePanel, "game");
+        this.setContentPane(root);
+        cardLayout.show(root, "menu");
+        pack();
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
+    }
+
+    public void StartGame() {
+        gamePanel = new GamePanel();
+        root.add(gamePanel, "game");
+        cardLayout.show(root, "game");
+        SwingUtilities.invokeLater(() -> gamePanel.requestFocusInWindow());
+        //System.out.println("I'm working");
+        root.validate();
     }
     
     public static void main(String[] args) {
-        new Window();
+        Window a = new Window();
     }
 }
